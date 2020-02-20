9e43c04eba9d3ac999ed97b04a273f589336b33e
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2020-02-20 07:52:59 +0530

    routing through pages

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class navbar extends Component {
+  render () {
+    return (
+      <div />
+    )
+  }
+}
+
+export default navbar
---
+++ src/pages/signup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class signup extends Component {
+  render () {
+    return (
+      <div><h1>signup page</h1></div>
+    )
+  }
+}
+
+export default signup
